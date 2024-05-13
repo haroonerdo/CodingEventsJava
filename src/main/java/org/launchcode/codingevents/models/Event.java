c7825a57fcc4fdf8958a24adb2b36cc7990f702e
haroonerdo <haroonerdo@gmail.com>
2024-05-13 18:32:58 -0500

    created 4 new field in Event class

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,9 +1,8 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 /**
@@ -25,11 +24,69 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    @NotBlank(message = "Location cannotbe left blank.")
+    private String location;
+
+    @AssertTrue(message = "Attendee must register for the event")
+    private boolean registrationRequired;
+
+    @Positive(message="Number of attendees must be one or more.")
+    private  int numberOfAttendees;
+
+    @NotNull(message = "Start time must not be null")
+    private LocalDateTime startTimeOfEvent;
+
+
+
+//    public Event(String name, String description, String contactEmail) {
+//        this();
+//        this.name = name;
+//        this.description = description;
+//        this.contactEmail = contactEmail;
+//    }
+
+
+    public Event(String name, String description, String contactEmail, String location, boolean registrationRequired, int numberOfAttendees, LocalDateTime startTimeOfEvent) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.location = location;
+        this.registrationRequired = registrationRequired;
+        this.numberOfAttendees = numberOfAttendees;
+        this.startTimeOfEvent = startTimeOfEvent;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+    public LocalDateTime getStartTimeOfEvent() {
+        return startTimeOfEvent;
+    }
+
+    public void setStartTimeOfEvent(LocalDateTime startTimeOfEvent) {
+        this.startTimeOfEvent = startTimeOfEvent;
     }
 
     public Event() {
